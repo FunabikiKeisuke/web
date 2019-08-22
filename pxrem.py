cc9ddbdefb06975b5634a9d28da62f6aa49d3e06
FunabikiKeisuke <47290651+FunabikiKeisuke@users.noreply.github.com>
2019-08-23 01:11:07 +0900

    add change_px files

---
+++ pxrem.py
@@ -0,0 +1,27 @@
+import sys
+import re
+# function format macro
+PX_NUM = 10000
+PX_REM= 10
+# command line arguments
+args = sys.argv
+# file open
+fr = open(sys.argv[1], 'r')
+fw = open(sys.argv[2], 'w')
+# file reading
+contents = fr.read()
+# variable declaration
+num = PX_NUM
+# unit conversion
+for i in range(PX_NUM):
+    if num == PX_NUM:
+        txt = (contents.replace(str(num) + 'px', str(float(num/PX_REM)) + 'rem'))
+        num -= 1
+    else:
+        txt = (txt.replace(str(num) + 'px', str(float(num/PX_REM)) + 'rem'))
+        num -= 1
+# file writing
+fw.write(txt)
+# file close
+fr.close()
+fw.close()
